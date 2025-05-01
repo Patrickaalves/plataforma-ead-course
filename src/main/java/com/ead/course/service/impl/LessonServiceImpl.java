dcd4aafb56c62306ca217934cf0c9eb22d3b3cad
Patrick <patrickalvessc@gmail.com>
2025-05-01 11:12:15 -0300

    Adicionando os repositories e os services

---
+++ src/main/java/com/ead/course/service/impl/LessonServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ead.course.service.impl;
+
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.service.LessonService;
+
+public class LessonServiceImpl implements LessonService {
+
+    private final LessonRepository lessonRepository;
+
+    public LessonServiceImpl(LessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+}
---
+++ src/main/java/com/ead/course/service/impl/ModuleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ead.course.service.impl;
+
+import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.service.ModuleService;
+
+public class ModuleServiceImpl implements ModuleService {
+    private final ModuleRepository moduleRepository;
+
+    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+        this.moduleRepository = moduleRepository;
+    }
+}
