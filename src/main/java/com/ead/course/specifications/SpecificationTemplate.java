d1b511e6aac6fe5fc0892ecfefd008c9fca060aa
Patrick <patrickalvessc@gmail.com>
2025-05-09 22:25:59 -0300

    Refatorando o specification

--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -4,6 +4,7 @@ import com.ead.course.model.CourseModel;
 import com.ead.course.model.LessonModel;
 import com.ead.course.model.ModuleModel;
 import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
@@ -37,35 +38,47 @@ public class SpecificationTemplate {
     public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
         return (root, query, cb) -> {
             query.distinct(true);
-            Root<ModuleModel> module = root;
             Root<CourseModel> course = query.from(CourseModel.class);
             Expression<Collection<ModuleModel>> courseModules = course.get("modules");
             return cb.and(
                  cb.equal(course.get("courseId"), courseId),
-                 cb.isMember(module, courseModules
-            ));
+                 cb.isMember(root, courseModules)
+            );
         };
     }
 
+    // Isso aqui serve para me trazer todos lessons que estao dentro do module e esses modules é buscado pelo param moduleId
     public static Specification<LessonModel> lessonModuleId(final UUID moduleId) {
-        return (root, query, cb) -> {
+        return (lessonModelRoot, query, cb) -> {
             query.distinct(true);
-            Root<LessonModel> lesson = root;
             Root<ModuleModel> module = query.from(ModuleModel.class);
             Expression<Collection<LessonModel>> moduleLessons = module.get("lessons");
             return cb.and(
                  cb.equal(module.get("moduleId"), moduleId),
-                 cb.isMember(lesson, moduleLessons
-            ));
+                 cb.isMember(lessonModelRoot, moduleLessons)
+            );
         };
     }
 
     public static Specification<LessonModel> teste() {
         return (root, query, cb) -> {
 
-          return cb.and(
+        String title = "teste";
+        String description = "description";
 
-          );
+        // select * from module
+        Root<ModuleModel> module = query.from(ModuleModel.class);
+
+        // prepare WHERE clause
+        // WHERE firstname like '%ali%'
+        Predicate firstNamePredicate = cb.like(module.get("title"), "%" + title + "%");
+        Predicate lastNamePredicate = cb.like(module.get("name"), "%" + description + "%");
+        Predicate orPredicate = cb.or(firstNamePredicate, lastNamePredicate);
+        // => final query ==> select * from module where title like '%teste%' or lastname like '%description%'
+
+        query.where(orPredicate);
+
+        return null;
         };
     }
 }
