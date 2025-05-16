84ea9626159bd76596afbf72a1a39c81ecaa0ea0
Patrick <patrickalvessc@gmail.com>
2025-05-16 17:15:33 -0300

    Adicionando configurações do rabbit mq

---
+++ src/main/java/com/ead/course/configs/RabbitmqConfig.java
@@ -0,0 +1,33 @@
+package com.ead.course.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    final CachingConnectionFactory cachingConnectionFactory; // permite que crie a configuração com o rabbit mq
+
+    public RabbitmqConfig(CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean // Para nao ter problemas relacionados a serializações/deserializacoes
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
